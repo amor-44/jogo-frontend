15aac7f3a2f725abd6d849e45a164f90878f6c0d
Amr Hesham <hamr6653@gmail.com>
2026-08-14 00:00:46 +0300

    Save AIChat changes before pull

--- Frontend/src/pages/AIChat/AIChat.tsx
+++ Frontend/src/pages/AIChat/AIChat.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
